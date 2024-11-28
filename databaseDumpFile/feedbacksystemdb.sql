496873a793cebfd90faa3d8b8ed56bcfaca6f722
joshuadaquila <joshuanomongdaquila@gmail.com>
2024-11-28 21:24:40 +0800

    added db dumpfile

---
+++ databaseDumpFile/feedbacksystemdb.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 28, 2024 at 02:23 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `feedbacksystemdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userId` int(11) NOT NULL,
+  `firstName` varchar(100) NOT NULL,
+  `middleName` varchar(100) NOT NULL,
+  `lastName` varchar(100) NOT NULL,
+  `extensionName` varchar(10) DEFAULT NULL,
+  `birthday` date NOT NULL,
+  `purok` varchar(10) NOT NULL,
+  `civilStatus` varchar(20) NOT NULL,
+  `educBgId` int(11) NOT NULL,
+  `userName` varchar(50) NOT NULL,
+  `userType` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(20) NOT NULL DEFAULT 'active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userId`, `firstName`, `middleName`, `lastName`, `extensionName`, `birthday`, `purok`, `civilStatus`, `educBgId`, `userName`, `userType`, `password`, `createdAt`, `status`) VALUES
+(1, 'John', 'Doe', 'Smith', '', '1990-01-01', '1', 'Single', 2, 'johnsmith', 'Admin', '$2b$10$I4C08jdJapByQG1Hgh/Chu4AtQTV97ksWt4NAKpj3.FNLp42Yg8Be', '2024-11-28 13:18:05', 'active');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
