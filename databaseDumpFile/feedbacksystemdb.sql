f7a3edeac1bc648a3975e65264e5448a63e13ea6
jayroselignig@gmail.com <jayroselignig@gmail.com>
2024-12-01 14:55:01 +0800

    Modified

--- databaseDumpFile/feedbacksystemdb.sql
+++ databaseDumpFile/feedbacksystemdb.sql
@@ -27,29 +27,29 @@ SET time_zone = "+00:00";
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `userId` int(11) NOT NULL,
-  `firstName` varchar(100) NOT NULL,
-  `middleName` varchar(100) NOT NULL,
-  `lastName` varchar(100) NOT NULL,
-  `extensionName` varchar(10) DEFAULT NULL,
+CCREATE TABLE `user` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(255) NOT NULL,
+  `middleName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `extensionName` varchar(50) DEFAULT NULL,
   `birthday` date NOT NULL,
-  `purok` varchar(10) NOT NULL,
-  `civilStatus` varchar(20) NOT NULL,
-  `educBgId` int(11) NOT NULL,
-  `userName` varchar(50) NOT NULL,
-  `userType` varchar(50) NOT NULL,
-  `password` varchar(500) NOT NULL,
+  `purok` varchar(255) NOT NULL,
+  `civilStatus` varchar(50) NOT NULL,
+  `userName` varchar(255) NOT NULL,
+  `userType` enum('admin', 'user') NOT NULL,
+  `password` varchar(255) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
-  `status` varchar(20) NOT NULL DEFAULT 'active'
+  `status` enum('active', 'inactive') NOT NULL DEFAULT 'active',
+  `educationBackground` varchar(255) NOT NULL,
+  PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`userId`, `firstName`, `middleName`, `lastName`, `extensionName`, `birthday`, `purok`, `civilStatus`, `educBgId`, `userName`, `userType`, `password`, `createdAt`, `status`) VALUES
-(1, 'John', 'Doe', 'Smith', '', '1990-01-01', '1', 'Single', 2, 'johnsmith', 'Admin', '$2b$10$I4C08jdJapByQG1Hgh/Chu4AtQTV97ksWt4NAKpj3.FNLp42Yg8Be', '2024-11-28 13:18:05', 'active');
+
 
 --
 -- Indexes for dumped tables
@@ -58,8 +58,6 @@ INSERT INTO `user` (`userId`, `firstName`, `middleName`, `lastName`, `extensionN
 --
 -- Indexes for table `user`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`userId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
